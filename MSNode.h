f916dfa907578a84367be4bc8359b50b17ac0af7
Callko12 <callko12@wfu.edu>
2016-01-05 04:14:02 -0500

    Create MSNode.h

---
+++ MSNode.h
@@ -0,0 +1,20 @@
+/*
+ *
+ * MS node
+ * 5/11/2014
+ *
+ */
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class MSNode {
+  public:
+
+  int count, addy;
+  bool xplode, seen;
+  string symbol;
+  
+};
